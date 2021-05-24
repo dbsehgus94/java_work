4ac21f8a9f95caf7a760bfab79867d579926a0a8
dbsehgus94 <ygsa885@naver.com>
2021-05-24 15:18:25 +0900

    21/05/24

---
+++ src/test/Test5_6.java
@@ -0,0 +1,13 @@
+package test;
+import java.util.Scanner;
+public class Test5_6 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.print("정수형 값: ");
+		int a = stdIn.nextInt();
+		a = (int) 10.0;
+
+	}
+
+}
